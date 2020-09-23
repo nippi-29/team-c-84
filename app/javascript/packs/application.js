b9ef56a9f118971651cd8940cb070db4b06cd942
nippi-29 <pipimelody.1726@gmail.com>
2020-09-23 13:14:45 +0900

    jqueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("jquery_ujs")
 require("../index")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
