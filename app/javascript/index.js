e882b7d913b2cd391c28468b2f2fc0f7a7e987e2
nippi-29 <pipimelody.1726@gmail.com>
2020-10-01 12:27:30 +0900

    音声の追加

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,5 +1,11 @@
 $(function() {
 
+  document.addEventListener('click', audioPlay);
+  function audioPlay() {
+  document.getElementById('audio').play();
+  document.removeEventListener('click', audioPlay);
+}
+
   $('#check_all').on("click",function(){
     $("input[name='person']").prop("checked", this.checked);
   });
@@ -15,11 +21,13 @@ $(function() {
     });
   $("#lottery").on("click", function() {
     const arr = [];
+    $("#audio")[0].pause();
 
     $("[name='person']:checked").each(function(){
       $(this).prop('disabled', true);
       arr.push($(this).val());
     });
+    $("#sound").get(0).play();
     const ary = arr[Math.floor(Math.random() * arr.length)];
     $("#span").html(ary+"さん、あなたです")
   });
